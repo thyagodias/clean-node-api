f32a4d813cd9764a84800bc4e1346e203bb99970
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-08-26 09:48:16 -0400

    chore: atualiza configurações do projeto

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir/src/**/*.ts>'],
+  collectCoverageFrom: [
+    '<rootDir/src/**/*.ts>',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest',
-  },
-};
+    '.+\\.ts$': 'ts-jest'
+  }
+}
